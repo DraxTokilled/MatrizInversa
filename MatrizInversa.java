5a6eab58a5c774c264ca16e4d55bf7b180e1cac4
DraxTokilled <sandroapolinar80@gmail.com>
2025-11-20 00:58:22 -0600

    Subida del proyecto Matriz Inversa

--- MatrizInversa.java
+++ MatrizInversa.java
@@ -1,5 +1,4 @@
 import java.io.*;
-import java.util.*;
 
 public class MatrizInversa {
 
@@ -8,24 +7,33 @@ public class MatrizInversa {
         BufferedReader br = new BufferedReader(new FileReader("matriz.txt"));
         double[][] m = new double[2][2];
 
-        m[0][0] = Double.parseDouble(br.readLine());
-        m[0][1] = Double.parseDouble(br.readLine());
-        m[1][0] = Double.parseDouble(br.readLine());
-        m[1][1] = Double.parseDouble(br.readLine());
+        // Leer matriz 2x2 desde archivo
+        m[0][0] = Double.parseDouble(br.readLine().trim());
+        m[0][1] = Double.parseDouble(br.readLine().trim());
+        m[1][0] = Double.parseDouble(br.readLine().trim());
+        m[1][1] = Double.parseDouble(br.readLine().trim());
         br.close();
 
+        // Determinante
         double det = m[0][0] * m[1][1] - m[0][1] * m[1][0];
 
+        if (det == 0) {
+            System.out.println("La matriz NO tiene inversa (determinante = 0).");
+            return;
+        }
+
+        // Matriz inversa
         double[][] inv = {
             {  m[1][1] / det, -m[0][1] / det },
             { -m[1][0] / det,  m[0][0] / det }
         };
 
+        // Guardar salida
         PrintWriter pw = new PrintWriter("matriz_inversa.txt");
         pw.println(inv[0][0] + " " + inv[0][1]);
         pw.println(inv[1][0] + " " + inv[1][1]);
         pw.close();
 
-        System.out.println("Inversa generada.");
+        System.out.println("Inversa generada. Revisa matriz_inversa.txt");
     }
 }
